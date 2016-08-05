075e9f2720d979913e75c235b81669cf7e5d244d
Alex Arriaga <alex.arriaga.m@gmail.com>
2016-08-05 12:46:29 -0500

    Add bootstrap to the application

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { PokeCatalogComponent } from "./poke-catalog/poke-catalog.component";
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1><poke-catalog></poke-catalog>',
+  template: '<poke-catalog></poke-catalog>',
   directives: [PokeCatalogComponent]
 })
 export class AppComponent {
